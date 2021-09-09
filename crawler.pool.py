d0de16441ad874d81e68aac3a4fa1b8e82288c60
cyanife <cyanife@gmail.com>
2021-09-09 18:57:24 +0900

    bugfix & performance

--- crawler.pool.py
+++ crawler.pool.py
@@ -33,6 +33,14 @@ class RoomInitError(Exception):
         self._msg = msg
         super().__init__(msg)
 
+    @property
+    def code(self):
+        return self._code
+
+    @property
+    def msg(self):
+        return self._msg
+
     def __str__(self) -> str:
         return f"[RoomInit] code: {self._code}, message: {self._msg}"
 
@@ -42,6 +50,14 @@ class LiveUserError(Exception):
         self._code = code
         self._msg = msg
         super().__init__(msg)
+
+    @property
+    def code(self):
+        return self._code
+
+    @property
+    def msg(self):
+        return self._msg
 
     def __str__(self) -> str:
         return f"[LiveUser] code: {self._code}, message: {self._msg}"
@@ -71,6 +87,12 @@ class Proxy:
     def punish_connectError(self):
         self._score -= 2
 
+    def discard(self):
+        self._score -= 3
+
+    def usable(self):
+        return self._score > 0
+
     def __str__(self) -> str:
         return f"[{self._host}:{self._port}]"
 
@@ -98,13 +120,19 @@ class Job:
     def fail(self):
         self._limit -= 1
 
+    def __lt__(self, other):
+        return self.rid < other.rid
+
 
 async def fetch(
     session: aiohttp.ClientSession,
     url: str,
 ) -> dict:
     async with session.get(url) as resp:
-        return await resp.json(loads=loads)
+        json = await resp.json(loads=loads)
+        # print(json)
+        return json
+        # return {"code": 0, "data": [{"host": "127.0.0.1", "port": "8080"}]}
 
 
 async def fetch_proxy(
@@ -113,8 +141,12 @@ async def fetch_proxy(
     params: dict[str, str],
     proxy: Proxy,
 ) -> dict:
-    async with session.get(url, params=params, proxy=proxy.url) as resp:
-        return await resp.json(loads=loads)
+    print(params)
+    # async with session.get(url, params=params, proxy=proxy.url) as resp:
+    async with session.get(url, params=params) as resp:
+        json = await resp.json(loads=loads)
+        # print(json)
+        return json
 
 
 async def get_room_info(
@@ -180,39 +212,45 @@ async def worker(
     session: aiohttp.ClientSession,
     conn: Connection,
     proxies: asyncio.PriorityQueue[Proxy],
-    jobs: asyncio.Queue[Job],
+    jobs: asyncio.PriorityQueue[Job],
 ):
     while True:
         async with sem:
             proxy = await proxies.get()
-            job = await jobs.get()
-            try:
-                if job.exhausted:
-                    print(f"Job failed. rid: {job.rid}")
-                    await proxies.put(proxy)
-                else:
-                    medal = await get_medal(session, job.rid, proxy)
-                    await save_result(conn, medal)
-                await proxies.put(proxy)
-            except TimeoutError:
-                proxy.punish_timeout()
-                await proxies.put(proxy)
-                await jobs.put(job)
-            except ClientConnectionError:
-                proxy.punish_connectError()
-                await proxies.put(proxy)
-                await jobs.put(job)
-            except ClientResponseError as e:
-                if e.status == 412:
-                    print(f"{str(proxy)} limit exceed.")
+            proxies.task_done()
+            if proxy.usable():
+                proxies.put_nowait(proxy)
+                job = await jobs.get()
+                try:
+                    if job.exhausted:
+                        print(f"Job failed. rid: {job.rid}")
+                    else:
+                        print(job.rid)
+                        medal = await get_medal(session, job.rid, proxy)
+                        print(medal)
+                        await save_result(conn, medal)
+                except TimeoutError:
+                    proxy.punish_timeout()
                     await jobs.put(job)
-                else:
-                    job.fail()
+                except ClientConnectionError:
+                    proxy.punish_connectError()
                     await jobs.put(job)
-                    await proxies.put(proxy)
-            finally:
-                proxies.task_done()
-                jobs.task_done()
+                except ClientResponseError as e:
+                    if e.status == 412:
+                        print(f"{str(proxy)} limit exceed.")
+                        proxy.discard()
+                        await jobs.put(job)
+                    else:
+                        job.fail()
+                        await jobs.put(job)
+                except RoomInitError as e:
+                    if e.code != 60004:  # room not exists
+                        print(f"{job.rid}: {e}")
+                except LiveUserError as e:
+                    print(f"{job.rid}: {e}")
+
+                finally:
+                    jobs.task_done()
 
 
 async def get_proxies(
@@ -235,8 +273,31 @@ async def get_proxy(
         data = proxy_res.get("data")
         for proxy_data in data:
             proxy = Proxy(proxy_data["host"], proxy_data["port"])
-            print(proxy)
             await proxies.put(proxy)
+
+
+async def distributer(
+    from_rid: int,
+    to_rid: int,
+    concurrency: int,
+    jobs: asyncio.PriorityQueue[Job],
+):
+    rid = from_rid
+    try:
+        while rid <= to_rid:
+            while not jobs.full():
+                if rid <= to_rid:
+                    job = Job(rid)
+                    jobs.put_nowait(job)
+                    rid += 1
+                else:
+                    break
+            await jobs.join()
+    except asyncio.CancelledError:
+        print(f"End at {rid}")
+    except Exception as e:
+        print(str(e))
+        print(f"Error occured at {rid}")
 
 
 async def main(
@@ -253,13 +314,13 @@ async def main(
         )
         await conn.commit()
 
-        sem = asyncio.Semaphore(concurrency)
+        sem = asyncio.Semaphore(concurrency + 2)
         connector = aiohttp.TCPConnector(
             ssl=False, limit=concurrency + 1, use_dns_cache=True
         )
 
-        proxies = asyncio.PriorityQueue()
-        jobs = asyncio.Queue(maxsize=concurrency)
+        proxies = asyncio.PriorityQueue(maxsize=concurrency * 2)
+        jobs = asyncio.PriorityQueue(maxsize=concurrency * 2)
 
         tasks = []
 
@@ -275,28 +336,36 @@ async def main(
         ) as session:
 
             try:
-                await get_proxy(session, proxies, proxy_api)
                 tasks.append(
                     asyncio.create_task(
                         get_proxies(session, proxies, proxy_api, frequency)
                     )
                 )
 
+                distributer_task = asyncio.create_task(
+                    distributer(from_rid, to_rid, concurrency, jobs)
+                )
+
+                tasks.append(distributer_task)
+
                 for i in range(concurrency):
                     tasks.append(
                         asyncio.create_task(worker(sem, session, conn, proxies, jobs))
                     )
 
-                while rid <= to_rid:
-                    while not jobs.full():
-                        if rid <= to_rid:
-                            job = Job(rid)
-                            jobs.put_nowait(job)
-                            rid += 1
-                        else:
-                            break
+                done, pending = await asyncio.wait(tasks, return_when="FIRST_COMPLETED")
+                try:
+                    for task in done:
+                        task.result()
                     await jobs.join()
-                    print(f"current:{rid}")
+                except Exception as e:
+                    print(str(e))
+                    print("Error occured")
+                print("finished!")
+
+            except KeyboardInterrupt:
+                distributer_task.cancel()
+
             except Exception as e:
                 print(str(e))
                 print(f"teminated at {rid}")
